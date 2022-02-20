3cbb13839e7b7964da2ad20fd04f6b3a5efb862a
Thuku7 <robethuku11@gmail.com>
2022-02-21 00:12:02 +0300

    Solve the errors encountered in testing credentials

--- credentials.py
+++ credentials.py
@@ -1,12 +1,11 @@
 class Credentials:
-    """
-    class that generates an instance of credentials
-    """
+    '''
+    class that generates an instance of credential 
+    '''
 
-    credential_requirments = []
+    credential_requirements = []
 
-
-    def __init__(self,social_name,social_user_name,social_user_password):
+    def __init__(self, social_name, social_user_name, social_user_password):
 
         self.social_name = social_name
         self.social_user_name = social_user_name
@@ -14,25 +13,54 @@ class Credentials:
 
 
     def save_credentials(self):
-        """
-        Method that saves credential object in the credential_requirements array
-
-        """
+        '''
+        Method that saves a credential object in the credential_requirements array
+        '''
 
         Credentials.credential_requirements.append(self)
 
     def delete_credentials(self):
-        """
+        '''
         Function that will delete a users credential
-        """ 
+        '''
 
-        Credentials.credential_requirements.remove(self)   
-
+        Credentials.credential_requirements.remove(self)
 
     @classmethod
-    def dispaly_credentials(cls):
-        """
+    def display_credentials(cls):
+        '''
         returns the credential list
-        """ 
+        '''
 
-        return cls.credential_requirements       
+        return cls.credential_requirements
+
+    @classmethod
+    def find_credentials(cls,social):
+        '''
+        Function that will take in a platform name and return the credentials that match
+        Args:
+            platform_name: name of the credential to search for
+        Return:
+            Credentials that match the platform_name
+        '''
+
+        for credential in cls.credential_requirements:
+            if credential.social_name == social:
+                return credential
+        
+    @classmethod
+    def credential_exists(cls, social):
+        '''
+        Function that will check for a credential exists from the credential list
+        Args:
+            Social: name to search if the credential exists
+        Returns:
+            Boolea: True or False
+            
+        '''
+
+        for credential in cls.credential_requirements:
+            if credential.social_name == social:
+                return True
+
+        return False
--- credentials_test.py
+++ credentials_test.py
@@ -1,42 +1,36 @@
 import unittest
 from credentials import Credentials
 
-
 class TestUser(unittest.TestCase):
-    """
-    Test case that defines test cases for the user class behaviour
-    """
-
+    '''
+    Test case that defines test cases for the usesr class behaviour
+    '''
 
     def setUp(self):
-        """
+        '''
         set up the method that will run before each test case
-        
-        """
-        self.new_credentials = Credentials("Instagram", "PUBG","987654")
+        '''
 
-
+        self.new_credentials = Credentials("9gag", "babavoss", "12345678")
 
     def test_init(self):
         '''
         test that the credential object is initialised as expected
         '''
 
-        self.assertEqual(self.new_credentials.platform_name, "Instagram")
-        self.assertEqual(self.new_credentials.platform_user_name, "PUBG")
-        self.assertEqual(self.new_credentials.platform_user_password, "997654")   
+        self.assertEqual(self.new_credentials.social_name, "9gag")
+        self.assertEqual(self.new_credentials.social_user_name, "babavoss")
+        self.assertEqual(self.new_credentials.social_user_password, "12345678")
 
 
     def test_save_credentials(self):
-        """
-        Test whether the credential object is saved in the cedential list
-        """
+        '''
+        Test whether the credential object is saved in the credential list
+        '''
 
         self.new_credentials.save_credentials()
-        self.assertEqual(len(Credentials.credential_requirements),1) 
+        self.assertEqual(len(Credentials.credential_requirements), 1)
 
-
-    
     def tearDown(self):
         '''
         Tear down method that cleans up after each test case has run
@@ -44,24 +38,47 @@ class TestUser(unittest.TestCase):
 
         Credentials.credential_requirements = []
 
-
     def test_Save_multiple_credentials(self):
         '''
         Test to check whether we can save multiple credentials to the array
         '''
 
         self.new_credentials.save_credentials()
-        test_credential = Credentials("instagram", "Brady", "09876")
+        test_credential = Credentials("instagram", "noob", "9876543")
         test_credential.save_credentials()
-        self.assertEqual(len(Credentials.credential_requirements), 2)  
-
+        self.assertEqual(len(Credentials.credential_requirements), 2)
 
     def test_display_all_credentials(self):
+        '''
+        Test whether all credentials are displayed back to the user
+        '''
 
-        """
-        Test whether all credentials are displayed back to user
-        """
+        self.assertEqual(Credentials.display_credentials(), Credentials.credential_requirements)
 
-        self.assertEqual(Credentials.display_credentials(), Credentials.credential_requirements)      
+    def test_delete_user_credentials(self):
+        '''
+        Test if a credential object is removed from the array
+        '''
 
-          
+        self.new_credentials.save_credentials()
+        test_credential = Credentials("Reddit", "Thuku", "123456")
+        test_credential.save_credentials()
+
+        self.new_credentials.delete_credentials()
+        self.assertEqual(len(Credentials.credential_requirements), 1)
+
+    def test_find_credential_by_name(self):
+        '''
+        Find credential by name
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credential = Credentials("Reddit", "Tom", "123456")
+        test_credential.save_credentials()
+
+        found_credential = Credentials.find_credentials("Reddit")
+        self.assertEqual(found_credential.social_name, test_credential.social_name)
+
+
+if __name__ == "__main__":
+    unittest.main()
