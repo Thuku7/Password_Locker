4d040a718cd2f60abcc2f0b0dfb2c3503e0d0354
Thuku7 <robethuku11@gmail.com>
2022-02-20 22:31:15 +0300

    Will create a method i the credentials class to save the credential array

--- credentials.py
+++ credentials.py
@@ -11,3 +11,12 @@ class Credentials:
         self.social_name = social_name
         self.social_user_name = social_user_name
         self.social_user_password = social_user_password
+
+
+    def save_credentials(self):
+        """
+        Method that saves credential object in the credential_requirements array
+
+        """
+
+        Credentials.credential_requirements.append(self)    
